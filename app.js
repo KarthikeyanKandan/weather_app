4d6fab10f9ce81c13d805bf394b8d53148d835ee
Karthikeyan <akashkarthiofficial@gmail.com>
2019-06-20 06:44:50 +0530

    js updation-5 (logical error recov)

--- app.js
+++ app.js
@@ -7,11 +7,18 @@ window.addEventListener('load', ()=>{
           long = position.coords.longitude;
             lat = position.coords.latitude;
             
-            const api=`https://api.darksky.net/forecast/76462b849293bf6babaa711474ef32ba/${lat},${long}`;
-        });
-        
+        const api=`https://api.darksky.net/forecast/76462b849293bf6babaa711474ef32ba/${lat},${long}`;
+            
         fetch(api)
-            .then(data)
-    }
+            .then(response =>{
+            return response.json();
+        })
+        .then(data =>{
+            console.log(data);
+        });
+            
+    });
+            
+}
     
 });
