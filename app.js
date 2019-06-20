4584fb10f617b61059e6d421673e1f46362e1e6c
Karthikeyan <akashkarthiofficial@gmail.com>
2019-06-20 12:06:30 +0530

    js updation-7 (final weather data ins)

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
-window.addEventListener('load', ()=>{
-   let long;
+window.addEventListener('load', () => {
+    let long;
     let lat;
+    let temperatureDescription = document.querySelector(".temperature-description");
+    let temperatureDegree = document.querySelector(".temperature-degree");
+    let locationTimezone = document.querySelector(".location-timezone");
     
     if (navigator.geolocation){
         navigator.geolocation.getCurrentPosition(position =>{
@@ -12,12 +15,15 @@ window.addEventListener('load', ()=>{
         const api=`${proxy}https://api.darksky.net/forecast/76462b849293bf6babaa711474ef32ba/${lat},${long}`;
             
         fetch(api)
-            .then(response =>{
+            .then(response => {
             return response.json();
         })
         .then(data =>{
             console.log(data);
             const { temperature, summary} = data.currently;
+            temperatureDegree.textContent = temperature;
+            temperatureDescription.textContent = summary;
+            locationTimezone.textContent = data.timezone;
             
         });
             
