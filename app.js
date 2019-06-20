24f47479d40593ce356937218e391543f1ec23cd
Karthikeyan <akashkarthiofficial@gmail.com>
2019-06-20 11:45:53 +0530

    js updation-5 (logical error recov)

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ window.addEventListener('load', ()=>{
    let long;
     let lat;
     
-    if(navigator.geolocation){
+    if (navigator.geolocation){
         navigator.geolocation.getCurrentPosition(position =>{
           long = position.coords.longitude;
             lat = position.coords.latitude;
